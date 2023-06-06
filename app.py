38c886acd64ef6414b19d4df35275d11922c67fa
RR <104732821+rrstats@users.noreply.github.com>
2023-06-06 21:03:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,9 +97,7 @@ def barchart(specs_dictionary):
                                    font=dict(size=23),
                                    font_family='Arial',
                                    font_color='#7E6E13',
-                                   autosize=True,
-                                    width=400,
-                                    height=400
+
                                    ),
 
                         xaxis_title=specs_dictionary["xaxis_title"],
